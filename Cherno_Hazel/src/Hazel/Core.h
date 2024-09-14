ba62e6ca30ce0998f9d41b8668fc68e59ca6e49f
悠木 <466040947@qq.com>
2024-09-14 18:15:53 +0800

    style(vscode): ensure correct macro definitions for VSCode preview

--- Cherno_Hazel/src/Hazel/Core.h
+++ Cherno_Hazel/src/Hazel/Core.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#ifdef VS_CODE_ONLY_DEFINE
+// 仅为了在 VSCode 中查看时代码提示正常，运行会报错
+	#define HZ_PLATFORM_WINDOWS
+	#define HZ_BUILD_DLL
+#endif
+
 #ifdef HZ_PLATFORM_WINDOWS
 	#ifdef HZ_BUILD_DLL
 		#define HAZEL_API __declspec(dllexport)
