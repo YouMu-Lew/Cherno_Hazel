beb5355d0fde320937819c4cbb692d120698b73a
YouMu <466040947@qq.com>
2025-03-11 11:55:53 +0800

    feat(WindowsWindow): 添加键盘和鼠标事件回调
    - 添加键盘按键按下、释放和重复事件的回调函数
    - 添加鼠标移动事件的回调函数
    - 添加鼠标滚动事件的回调函数

--- Cherno_Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Cherno_Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -74,6 +74,42 @@ namespace Hazel {
 				}
 			}
 			});
+
+		glfwSetKeyCallback(m_Window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+			WindowData& data = *static_cast<WindowData*>(glfwGetWindowUserPointer(window));
+
+			switch (action) {
+			case GLFW_PRESS: {
+				KeyPressedEvent event(key, 0);
+				data.EventCallback(event);
+				break;
+			}
+			case GLFW_RELEASE: {
+				KeyReleasedEvent event(key);
+				data.EventCallback(event);
+				break;
+			}
+			case GLFW_REPEAT: {
+				KeyPressedEvent event(key, 1);
+				data.EventCallback(event);
+				break;
+			}
+			}
+			});
+
+		glfwSetCursorPosCallback(m_Window, [](GLFWwindow* window, double xpos, double ypos) {
+			WindowData& data = *static_cast<WindowData*>(glfwGetWindowUserPointer(window));
+			
+			MouseMovedEvent event((float)xpos, (float)ypos);
+			data.EventCallback(event);
+			});
+
+		glfwSetScrollCallback(m_Window, [](GLFWwindow* window, double xoffset, double yoffset) {
+			WindowData& data = *static_cast<WindowData*>(glfwGetWindowUserPointer(window));
+
+			MouseScrolledEvent event((float)xoffset, (float)yoffset);
+			data.EventCallback(event);
+			});
 	}
 
 	void WindowsWindow::Shutdown() {
