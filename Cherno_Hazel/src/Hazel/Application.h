27e8e80f46e7963919a911300806a3a1e1285f4a
YouMu <lewyoumu@gmail.com>
2025-04-10 13:10:44 +0800

    Render a triangle using OpenGL

--- Cherno_Hazel/src/Hazel/Application.h
+++ Cherno_Hazel/src/Hazel/Application.h
@@ -34,6 +34,8 @@ namespace Hazel {
 		Hazel::ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		Hazel::LayerStack m_LayerStack;
+
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
